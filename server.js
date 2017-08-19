bc3000f8ce936671ada0dbf5682ac1f0884bf049
drishit96 <drishitmitra@gmail.com>
2017-08-19 22:26:40 +0530

    Updates to make server work on Heroku

--- server.js
+++ server.js
@@ -67,10 +67,9 @@ app.post('/busLocation', function(request, response) {
     }
 });
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-app.listen(server_port, server_ip_address, function () {
-    console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );
+var server_port = process.env.PORT || 8080;
+app.listen(server_port, function () {
+    console.log( "Listening on " + server_port  );
 });
 
 function sendNotification() {
